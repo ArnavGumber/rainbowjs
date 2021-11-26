aa68b3800a0b5d76603fdf07362ff2783fbb548b
Arnav Gumber <89778023+ArnavGumber@users.noreply.github.com>
2021-11-26 17:14:53 +0530

    Version 1.0 Commit

---
+++ rainbow.js
@@ -0,0 +1,264 @@
+//red
+var red = document.querySelectorAll("red");
+var red1;
+for (red1 = 0; red1 < red.length; red1++) {
+    red[red1].style.color = "red";
+}
+//blue
+var blue = document.querySelectorAll("blue");
+var blue1;
+for (blue1 = 0; blue1 < blue.length; blue1++) {
+    blue[blue1].style.color = "blue";
+}
+//green
+var green = document.querySelectorAll("green");
+var bgreen;
+for (green1 = 0; green1 < green.length; green1++) {
+    green[green1].style.color = "green";
+}
+//orange
+var orange = document.querySelectorAll("orange");
+var orange1;
+for (orange1 = 0; orange1 < orange.length; orange1++) {
+    orange[orange1].style.color = "orange";
+}
+//yellow
+var yellow = document.querySelectorAll("yellow");
+var yellow1;
+for (yellow1 = 0; yellow1 < yellow.length; yellow1++) {
+yellow[yellow1].style.color = "yellow";
+}
+//brown
+var brown = document.querySelectorAll("brown");
+var brown1;
+for (brown1 = 0; brown1 < brown.length; brown1++) {
+brown[brown1].style.color = "brown";
+}
+//pink
+var pink = document.querySelectorAll("pink");
+var pink1;
+for (pink1 = 0; pink1 < pink.length; pink1++) {
+pink[pink1].style.color = "pink";
+}
+//black
+var black = document.querySelectorAll("black");
+var black1;
+for (black1 = 0; black1 < black.length; black1++) {
+black[black1].style.color = "black";
+}
+//lime
+var lime = document.querySelectorAll("lime");
+var lime1;
+for (lime1 = 0; lime1 < lime.length; lime1++) {
+lime[lime1].style.color = "lime";
+}
+//cyan
+var cyan = document.querySelectorAll("cyan");
+var cyan1;
+for (cyan1 = 0; cyan1 < cyan.length; cyan1++) {
+cyan[cyan1].style.color = "cyan";
+}
+//aqua
+var aqua = document.querySelectorAll("aqua");
+var aqua1;
+for (aqua1 = 0; aqua1 < aqua.length; aqua1++) {
+aqua[aqua1].style.color = "aqua";
+}
+//chocolate
+var chocolate = document.querySelectorAll("chocolate");
+var chocolate1;
+for (chocolate1 = 0; chocolate1 < chocolate.length; chocolate1++) {
+chocolate[chocolate1].style.color = "chocolate";
+}
+//cornsilk
+var cornsilk = document.querySelectorAll("cornsilk");
+var cornsilk1;
+for (cornsilk1 = 0; cornsilk1 < cornsilk.length; cornsilk1++) {
+cornsilk[cornsilk1].style.color = "cornsilk";
+}
+//crimson
+var crimson = document.querySelectorAll("crimson");
+var crimson1;
+for (crimson1 = 0; crimson1 < crimson.length; crimson1++) {
+crimson[crimson1].style.color = "crimson";
+}
+//darkblue
+var darkblue = document.querySelectorAll("darkblue");
+var darkblue1;
+for (darkblue1 = 0; darkblue1 < darkblue.length; darkblue1++) {
+darkblue[darkblue1].style.color = "darkblue";
+}
+//darkcyan
+var darkcyan = document.querySelectorAll("darkcyan");
+var darkcyan1;
+for (darkcyan1 = 0; darkcyan1 < darkcyan.length; darkcyan1++) {
+darkcyan[darkcyan1].style.color = "darkcyan";
+}
+//darkgray
+var darkgray = document.querySelectorAll("darkgray");
+var darkgray1;
+for (darkgray1 = 0; darkgray1 < darkgray.length; darkgray1++) {
+darkgray[darkgray1].style.color = "darkgray";
+}
+//darkgreen
+var darkgreen = document.querySelectorAll("darkgreen");
+var darkgreen1;
+for (darkgreen1 = 0; darkgreen1 < darkgreen.length; darkgreen1++) {
+darkgreen[darkgreen1].style.color = "darkgreen";
+}
+//darkorange
+var darkorange = document.querySelectorAll("darkorange");
+var darkorange1;
+for (darkorange1 = 0; darkorange1 < darkorange.length; darkorange1++) {
+darkorange[darkorange1].style.color = "darkorange";
+}
+//darkred
+var darkred = document.querySelectorAll("darkred");
+var darkred1;
+for (darkred1 = 0; darkred1 < darkred.length; darkred1++) {
+darkred[darkred1].style.color = "darkred";
+}
+//deeppink
+var deeppink = document.querySelectorAll("deeppink");
+var deeppink1;
+for (deeppink1 = 0; deeppink1 < deeppink.length; deeppink1++) {
+deeppink[deeppink1].style.color = "deeppink";
+}
+//gold
+var gold = document.querySelectorAll("gold");
+var gold1;
+for (gold1 = 0; gold1 < gold.length; gold1++) {
+gold[gold1].style.color = "gold";
+}
+//gray
+var gray = document.querySelectorAll("gray");
+var gray1;
+for (gray1 = 0; gray1 < gray.length; gray1++) {
+gray[gray1].style.color = "gray";
+}
+//lightblue
+var lightblue = document.querySelectorAll("lightblue");
+var lightblue1;
+for (lightblue1 = 0; lightblue1 < lightblue.length; lightblue1++) {
+lightblue[lightblue1].style.color = "lightblue";
+}
+//lightcyan
+var lightcyan = document.querySelectorAll("lightcyan");
+var lightcyan1;
+for (lightcyan1 = 0; lightcyan1 < lightcyan.length; lightcyan1++) {
+lightcyan[lightcyan1].style.color = "lightcyan";
+}
+//lightgray
+var lightgray = document.querySelectorAll("lightgray");
+var lightgray1;
+for (lightgray1 = 0; lightgray1 < lightgray.length; lightgray1++) {
+lightgray[lightgray1].style.color = "lightgray";
+}
+//lightpink
+var lightpink = document.querySelectorAll("lightpink");
+var lightpink1;
+for (lightpink1 = 0; lightpink1 < lightpink.length; lightpink1++) {
+lightpink[lightpink1].style.color = "lightpink";
+}
+//lightyellow
+var lightyellow = document.querySelectorAll("lightyellow");
+var lightyellow1;
+for (lightyellow1 = 0; lightyellow1 < lightyellow.length; lightyellow1++) {
+lightyellow[lightyellow1].style.color = "lightyellow";
+}
+//magenta
+var magenta = document.querySelectorAll("magenta");
+var magenta1;
+for (magenta1 = 0; magenta1 < magenta.length; magenta1++) {
+magenta[magenta1].style.color = "magenta";
+}
+//maroon
+var maroon = document.querySelectorAll("maroon");
+var maroon1;
+for (maroon1 = 0; maroon1 < maroon.length; maroon1++) {
+maroon[maroon1].style.color = "maroon";
+}
+//mediumblue
+var mediumblue = document.querySelectorAll("mediumblue");
+var mediumblue1;
+for (mediumblue1 = 0; mediumblue1 < mediumblue.length; mediumblue1++) {
+mediumblue[mediumblue1].style.color = "mediumblue";
+}
+//navy
+var navy = document.querySelectorAll("navy");
+var navy1;
+for (navy1 = 0; navy1 < navy.length; navy1++) {
+navy[navy1].style.color = "navy";
+}
+//orangered
+var orangered = document.querySelectorAll("orangered");
+var orangered1;
+for (orangered1 = 0; orangered1 < orangered.length; orangered1++) {
+orangered[orangered1].style.color = "orangered";
+}
+//purple
+var purple = document.querySelectorAll("purple");
+var purple1;
+for (purple1 = 0; purple1 < purple.length; purple1++) {
+purple[purple1].style.color = "purple";
+}
+//royalblue
+var royalblue = document.querySelectorAll("royalblue");
+var royalblue1;
+for (royalblue1 = 0; royalblue1 < royalblue.length; royalblue1++) {
+royalblue[royalblue1].style.color = "royalblue";
+}
+//silver
+var silver = document.querySelectorAll("silver");
+var silver1;
+for (silver1 = 0; silver1 < silver.length; silver1++) {
+silver[silver1].style.color = "silver";
+}
+//skyblue
+var skyblue = document.querySelectorAll("skyblue");
+var skyblue1;
+for (skyblue1 = 0; skyblue1 < skyblue.length; skyblue1++) {
+skyblue[skyblue1].style.color = "skyblue";
+}
+//teal
+var teal = document.querySelectorAll("teal");
+var teal1;
+for (teal1 = 0; teal1 < teal.length; teal1++) {
+teal[teal1].style.color = "skyblue";
+}
+//tomato
+var tomato = document.querySelectorAll("tomato");
+var tomato1;
+for (tomato1 = 0; tomato1 < tomato.length; tomato1++) {
+tomato[tomato1].style.color = "tomato";
+}
+//violet
+var violet = document.querySelectorAll("violet");
+var violet1;
+for (violet1 = 0; violet1 < violet.length; violet1++) {
+violet[violet1].style.color = "violet";
+}
+//wheat
+var wheat = document.querySelectorAll("wheat");
+var wheat1;
+for (wheat1 = 0; wheat1 < wheat.length; wheat1++) {
+wheat[wheat1].style.color = "wheat";
+}
+//white
+var white = document.querySelectorAll("white");
+var white1;
+for (white1 = 0; white1 < white.length; white1++) {
+white[white1].style.color = "white";
+}
+//whitesmoke
+var whitesmoke = document.querySelectorAll("whitesmoke");
+var whitesmoke1;
+for (whitesmoke1 = 0; whitesmoke1 < whitesmoke.length; whitesmoke1++) {
+whitesmoke[whitesmoke1].style.color = "whitesmoke";
+}
+//yellowgreen
+var yellowgreen = document.querySelectorAll("yellowgreen");
+var yellowgreen1;
+for (yellowgreen1 = 0; yellowgreen1 < yellowgreen.length; yellowgreen1++) {
+yellowgreen[yellowgreen1].style.color = "yellowgreen";
+}
